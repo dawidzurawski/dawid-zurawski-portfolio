db3d40b0396bb6cf222ca446678ec6473719dfb1
dawidzurawski <dawidzurawskidrz@gmail.com>
2024-09-26 14:15:16 +0100

    Add navbar

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -1,4 +1,25 @@
+import { useState } from "react"
+import { navLinks } from "../constants"
+
+const NavItems = () => {
+  return (
+    <ul className="nav-ul">
+      {navLinks.map(({ id, href, name }) => (
+        <li key={id} className="nav-li">
+          <a href={href} className="nav-li_a" onClick={() => {}}>
+            {name}
+          </a>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
 const Navbar = () => {
+  const [isOpen, setOpen] = useState(false)
+
+  const toggleMenu = () => setOpen((prevIsOpen) => !prevIsOpen)
+
   return (
     <header className="fixed top-0 left-0 right-0 z-50 bg-black/90">
       <div className="max-w-7xl mx-auto">
@@ -7,13 +28,27 @@ const Navbar = () => {
             Dawid
           </a>
 
-          <button>
+          <button 
+            onClick={toggleMenu}
+            className="text-neutral-400 hover:text-white focus:outline-none sm:hidden flex"
+            aria-label="Toggle menu"
+          >
             <img
-              src={"assets/menu.svg"}
+              src={isOpen ? "assets/close.svg" : "assets/menu.svg"}
               alt="toggle"
               className="w-6 h-6"
-            />
+            />         
           </button>
+
+          <nav className="sm:flex hidden">
+            <NavItems />
+          </nav>
+        </div>
+
+        <div className={`nav-sidebar ${isOpen ? 'max-h-screen' : 'max-h-0'}`}>
+          <nav className="p-5">
+            <NavItems />
+          </nav>
         </div>
       </div>
     </header>
