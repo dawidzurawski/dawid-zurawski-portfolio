806ae428e39e095b426e9ded2b41c24297ef5bd4
dawidzurawski <dawidzurawskidrz@gmail.com>
2024-09-27 09:12:36 +0100

    Add hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -2,9 +2,11 @@ import { PerspectiveCamera, Ring } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import React, { Suspense } from 'react'
 import { useMediaQuery } from 'react-responsive'
+import Button from '../components/Button'
 import CanvasLoader from '../components/CanvasLoader'
 import Cube from '../components/Cube'
 import HackerRoom from '../components/HackerRoom'
+import HeroCamera from '../components/HeroCamera'
 import ReactLogo from '../components/ReactLogo'
 import Rings from '../components/Rings'
 import Target from '../components/Target'
@@ -22,31 +24,40 @@ const Hero = () => {
       <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
         <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">Hi, I am Dawid <span className="waving-hand">👋🏼</span></p>
         <p className="hero_tag text-gray_gradient">Building Products & Brands</p>
+      </div>
 
-        <div className="w-full h-full absolute inset-0">
-          {/* <Leva /> */}
-          <Canvas className="w-full h-full">
-            <Suspense fallback={<CanvasLoader />}>
-              <PerspectiveCamera makeDefault position={[0, 0, 20]} />
+      <div className="w-full h-full absolute inset-0">
+        {/* <Leva /> */}
+        <Canvas className="w-full h-full">
+          <Suspense fallback={<CanvasLoader />}>
+            <PerspectiveCamera makeDefault position={[0, 0, 20]} />
+
+            <HeroCamera isMobile={isMobile}>
               <HackerRoom 
                 position={sizes.deskPosition}
                 rotation={[0, -Math.PI, 0]}
                 scale={sizes.deskScale}
-              />
+                />
+            </HeroCamera>
 
-              <group>
-                <Target position={sizes.targetPosition} />
-                <ReactLogo position={sizes.reactLogoPosition} />
-                <Cube position={sizes.cubePosition} />
-                <Rings position={sizes.ringPosition} />
-              </group>
+            <group>
+              <Target position={sizes.targetPosition} />
+              <ReactLogo position={sizes.reactLogoPosition} />
+              <Cube position={sizes.cubePosition} />
+              <Rings position={sizes.ringPosition} />
+            </group>
 
-              <ambientLight intensity={1} />
-              <directionalLight position={[10, 10, 10]} intensity={0.5} />
-            </Suspense>
-          </Canvas>
-        </div>
-      </div>
+            <ambientLight intensity={1} />
+            <directionalLight position={[10, 10, 10]} intensity={0.5} />
+          </Suspense>
+        </Canvas>
+    </div>
+
+    <div className="absolute bottom-7 left-0 right-0 w-full z-10 c-space">
+      <a href="#contact" className="w-fit">
+        <Button name="Let's work rogether" isBeam containerClass="sm:w-fit w-full sm:min-w-96" />
+      </a>
+    </div>
     </section>
   )
 }
