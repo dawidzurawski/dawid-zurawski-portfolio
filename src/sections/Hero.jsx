a05c1c50d964d560c2df7a4169755d323873db32
dawidzurawski <dawidzurawskidrz@gmail.com>
2024-09-27 08:00:22 +0100

    Add animations

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,9 +1,22 @@
-import { PerspectiveCamera } from '@react-three/drei'
+import { PerspectiveCamera, Ring } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
-import React from 'react'
+import React, { Suspense } from 'react'
+import { useMediaQuery } from 'react-responsive'
+import CanvasLoader from '../components/CanvasLoader'
+import Cube from '../components/Cube'
 import HackerRoom from '../components/HackerRoom'
+import ReactLogo from '../components/ReactLogo'
+import Rings from '../components/Rings'
+import Target from '../components/Target'
+import { calculateSizes } from '../constants'
 
 const Hero = () => {
+  const isSmall = useMediaQuery({ maxWidth: 440 })
+  const isMobile = useMediaQuery({ maxWidth: 768 })
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 })
+
+  const sizes = calculateSizes(isSmall, isMobile, isTablet)
+
   return (
     <section className="min-h-screen w-full flex flex-col relative">
       <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
@@ -11,14 +24,33 @@ const Hero = () => {
         <p className="hero_tag text-gray_gradient">Building Products & Brands</p>
 
         <div className="w-full h-full absolute inset-0">
+          {/* <Leva /> */}
           <Canvas className="w-full h-full">
-            <PerspectiveCamera makeDefault position={[0, 0, 30]} />
-            <HackerRoom />
+            <Suspense fallback={<CanvasLoader />}>
+              <PerspectiveCamera makeDefault position={[0, 0, 20]} />
+              <HackerRoom 
+                position={sizes.deskPosition}
+                rotation={[0, -Math.PI, 0]}
+                scale={sizes.deskScale}
+              />
+
+              <group>
+                <Target position={sizes.targetPosition} />
+                <ReactLogo position={sizes.reactLogoPosition} />
+                <Cube position={sizes.cubePosition} />
+                <Rings position={sizes.ringPosition} />
+              </group>
+
+              <ambientLight intensity={1} />
+              <directionalLight position={[10, 10, 10]} intensity={0.5} />
+            </Suspense>
           </Canvas>
         </div>
       </div>
     </section>
   )
 }
+
+            
 
 export default Hero
